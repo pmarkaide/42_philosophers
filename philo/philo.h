db80da0eebe91a890835ca82cdd684168eb5dde4
Pablo Markaide <pablo.markaide@gmail.com>
2024-09-16 12:11:15 +0300

    ✨ Add basic header file and main.c

---
+++ philo/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 12:01:07 by pmarkaid          #+#    #+#             */
+/*   Updated: 2024/09/16 12:10:07 by pmarkaid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdlib.h>        // memset, malloc, free
+# include <stdio.h>         // printf
+# include <unistd.h>        // write, usleep
+# include <sys/time.h>      // gettimeofday
+# include <pthread.h>       // pthread functions
+
+#endif /* PHILO_H */
